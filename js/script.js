884ea3862288e396190e4be40f51e33c35f58dfa
Pippin Barr <pippin.barr@gmail.com>
2018-11-20 07:40:28 -0500

    Began work on implementing "real" game
    ? Started a fresh JSON data file with text from the original Twine. This included some experimentation with formats like splitting out exits and commands, before giving up on that. For now I do have an "expression" associated with each passage of text which indicates whether or not that passage of text should be displayed, but I worry it's a weird muddling of the script and the data, so might get rid of it later. The problem there is around texts that should only display at certain passage counts (like the first time text in CT Spawn)
    
    ? Changed the CSS to match the Twine's styling. It does look better I think.
    
    ? Playing with animating the scroll so that the current part of the story automatically scrolls into view, as this feels in keeping with it reacting to voice. For now it doesn't work the right way, but in principle this will be possible once I understand it better.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 /*
 
-Untitled Voice Shooter
+"I shoot"
 Pippin Barr
 
 */
 
 // Track current location (in world and in JSON)
-var currentPlace = 'longCorner';
+let currentPlace = 'CT_Spawn';
 // To store the object that comes from JSON
-var places;
+let data;
 // To number passages as we display them for jQuery effects
-var passage = 0;
+let passage = 0;
 // Attempts made at the current command set
 const MAX_ATTEMPTS = 1;
-var attempts = 0;
+let attempts = 0;
 // For a reference to the game text
-var $text;
+let $text;
 
 const PASSAGE_FADE_IN_TIME = 1000;
 const COMMAND_FADE_OUT_TIME = 500;
@@ -47,10 +47,10 @@ function onDataFailed() {
 // onDataLoaded()
 //
 // Store the data in our variable and start displaying the game
-function onDataLoaded(data) {
-  console.log('Game data loaded.');
+function onDataLoaded(loadedData) {
+  console.log('>>> Game data loaded.');
 
-  places = data;
+  data = loadedData;
   displayCurrentPassage();
 }
 
@@ -60,14 +60,19 @@ function onDataLoaded(data) {
 // displays the passage description and commands
 function displayCurrentPassage() {
 
+  // Increment passage counter
+  passage++;
+
   // Reset annyang's commands
   annyang.removeCommands();
 
   // Go through the description paragraph by paragraph and add to the page
-  for (let i = 0; i < places[currentPlace].description.length; i++) {
+  for (let i = 0; i < data[currentPlace].description.length; i++) {
+    if (!eval(data[currentPlace].description[i].test)) continue;
     let $p = $('<p></p>');
     $p.addClass('passage' + passage);
-    $p.append(places[currentPlace].description[i]);
+    $p.addClass('text' + passage);
+    $p.append(data[currentPlace].description[i].text);
     $text.append($p);
   }
 
@@ -76,41 +81,42 @@ function displayCurrentPassage() {
   let $commands = $('<div></div>');
 
   // Go through all the commands
-  for (let i = 0; i < places[currentPlace].commands.length; i++) {
-
+  for (let i = 0; i < data[currentPlace].commands.length; i++) {
     // Store the components nicely
-    let command = places[currentPlace].commands[i].command;
-    let destination = places[currentPlace].commands[i].destination;
-    let description = places[currentPlace].commands[i].description;
+    let command = data[currentPlace].commands[i].command;
+    let destination = data[currentPlace].commands[i].destination;
+    let display = data[currentPlace].commands[i].display;
 
     // Add each command to our object
     annyangCommands[command] = function () {
       // Handler function should know destination and description of command
       // for moving through story and for jQuery effects
-      executeCommand(destination,description);
+      executeCommand(destination,text);
     }
-
-    // Using the splat tag thing seems to sabotage the actual commands?
-    // annyangCommands['*words'] = function(words) {
-    // console.log('I heard: ' + words);
-    // }
 
     // Build the display version of the commands
     let $command = $('<p></p>');
-    $command.addClass('command passage' + passage);
+    $command.addClass('command-' + passage);
+    $command.addClass('command');
     $command.attr('id',destination);
-    $command.append(description);
+    $command.append(`"${display}."`);
     $commands.append($command);
   }
 
   // Add the commands to the page
-  $('#text').append($commands);
+  $text.append($commands);
+
+  $('html, body').animate({
+    scrollTop: $('.text' + passage).offset().top
+  },500);
 
   // Fade this passage in
-  $('.passage' + passage).hide().fadeIn(PASSAGE_FADE_IN_TIME);
+  $('.command-' + passage).hide();
+  $('.text' + passage).hide().fadeIn(PASSAGE_FADE_IN_TIME,function() {
+    $('.command-' + passage).fadeIn(PASSAGE_FADE_IN_TIME,function() {
 
-  // Increment passage counter
-  passage++;
+    });
+  });
 
   // Now we've defined the commands we give them to annyang
   // by using its .addCommands() function.
@@ -126,14 +132,14 @@ function displayCurrentPassage() {
 //
 // Does some nice jQuery animation to transition
 // and moves to the requested passage based on the command
-function executeCommand(commandDestination,commandDescription) {
-  console.log('Executing command to destination: ' + commandDestination);
+function executeCommand(destination,description) {
+  console.log('>>> Executing command to: ' + destination);
 
   // Update current place
-  currentPlace = commandDestination;
+  currentPlace = destination;
 
   // Fade out and slide up all the commands that weren't the one issued
-  $('.command').not('#' + commandDestination).animate({
+  $('.command').not('#' + destination).animate({
     opacity: 0
   }, COMMAND_FADE_OUT_TIME, function () {
     $(this).slideUp(COMMAND_SLIDE_UP_TIME, function () {
@@ -143,8 +149,8 @@ function executeCommand(commandDestination,commandDescription) {
 
   // Change the text of the command issues to be regular text
   // and stop it being a command for that destination
-  $('#' + commandDestination).removeClass('command');
-  $('#' + commandDestination).removeAttr('id');
+  $('#' + destination).removeClass('command');
+  $('#' + destination).removeAttr('id');
 
   // Reset attempts now that we've successfully issued a command
   attempts = 0;
@@ -166,15 +172,16 @@ function handleMishearing(possibles) {
 }
 
 function makeCommandsClickable() {
-  for (let i = 0; i < places[currentPlace].commands.length; i++) {
+  for (let i = 0; i < data[currentPlace].commands.length; i++) {
     // Store the components nicely
-    let command = places[currentPlace].commands[i].command;
-    let destination = places[currentPlace].commands[i].destination;
-    let description = places[currentPlace].commands[i].description;
+    let command = data[currentPlace].commands[i].command;
+    let destination = data[currentPlace].commands[i].destination;
+    let description = data[currentPlace].commands[i].description;
 
+    // Style the element as clickable
     $('#' + destination).addClass('clickable');
+    // Add a click event that executes its command and makes it unclickable
     $('#' + destination).on('click', function () {
-      console.log("click");
       executeCommand(destination,description);
       $(this).off('click');
       $(this).removeClass('clickable',1000);
