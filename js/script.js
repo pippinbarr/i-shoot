3575a666fc8df5575ab280ba72f04dd769a896b5
Pippin Barr <pippin.barr@gmail.com>
2018-11-22 07:19:51 -0500

    Refactoring code to make things hold together more solidly and be more readable (in theory)
    ? Not really connected with design thinking obviously, more connected with vague uneasiness about shaky code that might fall to pieces and be hard to debug. So at least now the whole thing feels better organised and read to add in further features without going insane?

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
       map = loadedData;
       $.getJSON('data/encounters.json','',function (loadedData) {
         encounters = loadedData;
-        displayCurrentPassage();
+        move({destination: currentPlace});
       }).fail(onDataFailed);
     })
     .fail(onDataFailed);
@@ -53,154 +53,7 @@ function onDataFailed() {
   console.log('Shitcakes.');
 }
 
-// displayCurrentPassage()
-//
-// Sets up annyang with the commands for this passage,
-// displays the passage description and commands
-function displayCurrentPassage() {
-
-  // Increment passage counter
-  passage++;
-
-  $passage = $('<div></div>');
-
-  // Go through the description paragraph by paragraph and add to the page
-  for (let i = 0; i < map[currentPlace].description.length; i++) {
-    let $p = $('<p></p>');
-    $p.addClass(`passage-${passage}`);
-    $p.addClass(`text-${passage}`);
-    $p.append(map[currentPlace].description[i].text);
-    $passage.append($p);
-  }
-
-  // Add any dynamic text this location has if the test expression is true
-  if (map[currentPlace].hasOwnProperty('dynamic') && eval(map[currentPlace].dynamic.test)) {
-    let $p = $('<p></p>');
-    $p.addClass(`passage-${passage}`);
-    $p.addClass(`text-${passage}`);
-    $p.append(map[currentPlace].dynamic.text);
-    $passage.append($p);
-  }
-
-  // Build our annyang commands and the display version
-  let annyangCommands = {};
-  let $commands = $('<div></div>');
-
-  // Go through all the commands
-  let commands = map[currentPlace].commands;
-  for (let i = 0; i < commands.length; i++) {
-    // Store the components nicely
-    let command = commands[i].command.toLowerCase();
-    let destination = commands[i].destination;
-    let display = commands[i].command;
-
-    // Build the display version of the command
-    let $command = $('<p></p>');
-    $command.addClass(`command-${passage}`);
-    $command.addClass('command');
-    $command.attr('id',destination);
-    $command.append(`"${display}."`);
-
-    if (makeClickable) {
-      // Style the element as clickable
-      $command.addClass('clickable');
-      // Add a click event that executes its command and makes it unclickable
-      $command.on('click', function () {
-        execute($command,move,{destination: destination});
-        $(this).off('click');
-        $(this).removeClass('clickable');
-      });
-    }
-
-    $commands.append($command);
-
-    // Add the command to our object
-    annyangCommands[command] = function () {
-      // Handler function should know destination and description of command
-      // for moving through story and for jQuery effects
-      execute($command,move,{destination: destination});
-    }
-
-  }
-
-  // Add an encounter if necessary
-  let r = Math.random();
-  if (r < 1.0) {
-    let $p = $('<p></p>');
-    $p.addClass(`passage-${passage}`);
-    $p.addClass(`text-${passage}`);
-    $p.append(encounters["T1"].description);
-    $passage.append($p);
-
-    let $command = $('<p></p>');
-    $command.addClass(`command-${passage}`);
-    $command.addClass('command');
-    $command.attr('id',"T1-"+0);
-
-    let killCommands = encounters["T1"].commands;
-    let killIndex = 0;
-
-    let firstCommand = killCommands[killIndex];
-    $command.append(`"${firstCommand}."`);
-
-    if (makeClickable) {
-      // Style the element as clickable
-      $command.addClass('clickable');
-      // Add a click event that executes its command and makes it unclickable
-      $command.on('click', function () {
-        execute($command,kill,{steps:killCommands,index:killIndex});
-        $(this).off('click');
-        $(this).removeClass('clickable');
-      });
-    }
-
-    $commands.append($command);
-
-    annyangCommands[firstCommand] = function () {
-      execute($command,kill,{steps:killCommands,index:killIndex});
-    }
-  }
-
-  // Add everything to the page
-  $text.append($passage);
-  $text.append($commands);
-
-  $passage.css('opacity',0);
-  $commands.css('opacity',0);
-
-  if ($commands.offset().top + $commands.height() > $(window).height()) {
-    scrollToPassage();
-  }
-  else {
-    fadeInPassage();
-  }
-
-  function scrollToPassage() {
-    $('html, body').animate({
-      scrollTop: $commands.offset().top + $commands.height()
-    },1000,fadeInPassage);
-  }
-
-  function fadeInPassage() {
-    $passage.animate({opacity:1},PASSAGE_FADE_IN_TIME,function() {
-      $commands.animate({opacity:1},PASSAGE_FADE_IN_TIME,function() {
-      });
-    });
-
-  }
-
-  // Reset annyang's commands
-  annyang.removeCommands();
-
-  // Now we've defined the commands we give them to annyang
-  // by using its .addCommands() function.
-  annyang.addCommands(annyangCommands);
-
-  // Set up for mishearings
-  annyang.addCallback('resultNoMatch', handleMishearing);
-}
-
-// executeCommand()
+// execute()
 //
 // Does some nice jQuery animation to transition
 // and moves to the requested passage based on the command
@@ -248,85 +101,170 @@ function move(data) {
   // Reset attempts now that we've successfully issued a command
   attempts = 0;
 
-  displayCurrentPassage();
+  // Increment passage counter
+  passage++;
+
+  // Create the overall passage
+  $passage = $('<div></div>');
+
+  // Go through the description paragraph by paragraph and add to the page
+  for (let i = 0; i < map[currentPlace].description.length; i++) {
+    let $p = $('<p></p>');
+    $p.addClass(`passage-${passage}`);
+    $p.addClass(`text-${passage}`);
+    $p.append(map[currentPlace].description[i].text);
+    $passage.append($p);
+  }
+
+  // Add any dynamic text this location has if the test expression is true
+  if (map[currentPlace].hasOwnProperty('dynamic') && eval(map[currentPlace].dynamic.test)) {
+    let $p = $('<p></p>');
+    $p.addClass(`passage-${passage}`);
+    $p.addClass(`text-${passage}`);
+    $p.append(map[currentPlace].dynamic.text);
+    $passage.append($p);
+  }
+
+  // Build our annyang commands and the display version
+  let annyangCommands = {};
+  let $commands = $('<div></div>');
+
+  // Go through all the commands
+  let commands = map[currentPlace].commands;
+  for (let i = 0; i < commands.length; i++) {
+    // Store the components nicely
+    let command = commands[i].command;
+    let destination = commands[i].destination;
+    let $command = buildCommand({command:command,id:destination},move,{destination: destination});
+    $commands.append($command);
+
+    // Add the command to our object
+    annyangCommands[command.toLowerCase()] = function () {
+      // Handler function should know destination and description of command
+      // for moving through story and for jQuery effects
+      execute($command,move,{destination: destination});
+    }
+  }
+
+  // Add an encounter if necessary
+  if (Math.random() < 1.0) {
+    addTerroristEncounter($passage,$commands,annyangCommands);
+  }
+
+  // Add everything to the page
+  $text.append($passage);
+  $text.append($commands);
+
+  scrollToCommands($passage,$commands);
+  setAnnyangCommands(annyangCommands);
+}
+
+function addTerroristEncounter($passage,$commands,annyangCommands) {
+  let $p = $('<p></p>');
+  $p.addClass(`passage-${passage}`);
+  $p.addClass(`text-${passage}`);
+  $p.append(encounters["T1"].description);
+  $passage.append($p);
+
+  let id = "T1-0"
+
+  let commandText = encounters["T1"].commands[0];
+  let data = {
+    commands: encounters["T1"].commands,
+    index: 0
+  };
+  let $command = buildCommand({
+    id: id,
+    command: commandText
+  },kill,data);
+
+  $commands.append($command);
+
+  annyangCommands[commandText.toLowerCase()] = function () {
+    execute($command,kill,{commands:encounters["T1"].commands,index:0});
+  }
 }
 
 function kill(data) {
-  let steps = data.steps;
-  let index = data.index;
+  passage++;
+  data.index++;
 
-  index++;
-
-  if (index === steps.length) {
-    move({destination:currentPlace});
+  if (data.index === data.commands.length) {
+    move({ destination:currentPlace });
   }
   else {
-    // Increment passage counter
-    passage++;
-
-    let $commands = $('<div></div>');
-
-    let $command = $('<p></p>');
-    $command.addClass(`command-${passage}`);
-    $command.addClass('command');
-    $command.attr('id',"T1-"+index);
-
-    let killCommands = steps;
-    let killIndex = index;
-
-    let nextCommand = killCommands[killIndex];
-    $command.append(`"${nextCommand}."`);
-
-    if (makeClickable) {
-      // Style the element as clickable
-      $command.addClass('clickable');
-      // Add a click event that executes its command and makes it unclickable
-      $command.on('click', function () {
-        execute($command,kill,{steps:killCommands,index:killIndex});
-        $(this).off('click');
-        $(this).removeClass('clickable');
-      });
-    }
-
-    $commands.append($command);
-    $text.append($commands);
-
-    $commands.css('opacity',0);
-
-    if ($commands.offset().top + $commands.height() > $(window).height()) {
-      scrollToPassage();
-    }
-    else {
-      fadeInPassage();
-    }
-
-    function scrollToPassage() {
-      $('html, body').animate({
-        scrollTop: $commands.offset().top + $commands.height()
-      },1000,fadeInPassage);
-    }
-
-    function fadeInPassage() {
-      $passage.animate({opacity:1},PASSAGE_FADE_IN_TIME,function() {
-        $commands.animate({opacity:1},PASSAGE_FADE_IN_TIME,function() {
-        });
-      });
-    }
-
     let annyangCommands = {};
-    annyangCommands[nextCommand.toLowerCase()] = function () {
-      execute($command,kill,{steps:killCommands,index:killIndex});
+    let command = { command: data.commands[data.index], id: "T1-"+passage };
+    let $passage = $('<div></div>');
+    let $commands = $('<div></div>');
+    let $command = buildCommand(command,kill,data);
+    $commands.append($command);
+    annyangCommands[$command.text().toLowerCase()] = function () {
+      execute($command,kill,data);
     }
+    $text.append($commands);
+    setAnnyangCommands(annyangCommands);
+    scrollToCommands($passage,$commands);
+  }
+}
 
-    // Reset annyang's commands
-    annyang.removeCommands();
+function setAnnyangCommands(annyangCommands) {
+  // Reset annyang's commands
+  annyang.removeCommands();
 
-    // Now we've defined the commands we give them to annyang
-    // by using its .addCommands() function.
-    annyang.addCommands(annyangCommands);
+  // Now we've defined the commands we give them to annyang
+  // by using its .addCommands() function.
+  annyang.addCommands(annyangCommands);
 
-    // Set up for mishearings
-    annyang.addCallback('resultNoMatch', handleMishearing);
+  // Set up for mishearings
+  annyang.addCallback('resultNoMatch', handleMishearing);
+}
+
+function buildCommand(command,handler,data) {
+  console.log(`Building ${command.id}: ${command.command}`)
+  let $command = $('<p></p>');
+  $command.addClass(`command-${passage}`);
+  $command.addClass('command');
+  $command.attr('id',command.id);
+
+  $command.append(`"${command.command}."`);
+
+  if (makeClickable) {
+    // Style the element as clickable
+    $command.addClass('clickable');
+    // Add a click event that executes its command and makes it unclickable
+    $command.on('click', function () {
+      execute($command,handler,data);
+      $(this).off('click');
+      $(this).removeClass('clickable');
+    });
+  }
+
+  return $command;
+}
+
+function scrollToCommands($passage,$commands) {
+  $passage.css('opacity',0);
+  $commands.css('opacity',0);
+
+  if ($commands.offset().top + $commands.height() > $(window).height()) {
+    scrollToNext();
+  }
+  else {
+    fadeInNext();
+  }
+
+  function scrollToNext() {
+    $('html, body').animate({
+      scrollTop: $commands.offset().top + $commands.height()
+    },1000,fadeInNext);
+  }
+
+  function fadeInNext() {
+    $passage.animate({opacity:1},PASSAGE_FADE_IN_TIME,function() {
+      $commands.animate({opacity:1},PASSAGE_FADE_IN_TIME,function() {
+      });
+    });
   }
 }
 
